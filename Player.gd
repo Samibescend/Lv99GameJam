6981d98c1d1e71e3f1c3bc21adfff1e1e72de601
Oreobliton <tristanpicard99@gmail.com>
2023-04-15 14:45:32 +0200

    [WIP] trying to implement focus

--- Player.gd
+++ Player.gd
@@ -1,11 +1,33 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var focus = 0
+
+func _ready():
+	set_process_input(true) 
+
+
+func _input(event):
+	if event.is_action("charge_focus"):
+		charge_focus()
+
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
+
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+
+
+func charge_focus():
+	$ChargeTimer.start()
+
+
+
+func _on_charge_timer_timeout():
+	focus += 1
+	print(focus)
+
