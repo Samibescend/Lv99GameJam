94b2e81e46e55cd5815272c364b2d6120d45e097
allez100dreau <aless.pep@hotmail.fr>
2023-04-15 15:25:16 +0200

    Dash

--- Player.gd
+++ Player.gd
@@ -1,11 +1,24 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var dash_speed = 1000
+
+var old_speed
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
-
+	
+	if (Input.is_action_just_pressed("dash") and $DashTimer.is_stopped()):
+		old_speed = speed
+		speed += dash_speed
+		$DashTimer.start()
+	
+	
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+
+
+func _on_dash_timer_timeout():
+	speed = old_speed
