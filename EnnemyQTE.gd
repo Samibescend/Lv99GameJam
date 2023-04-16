1d951f256e48380fa4b6348d155bd4c4f4e11412
Zorrino <pierrekouyoumdjian@hotmail.fr>
2023-04-16 17:04:28 +0200

    Travelling + zoom

--- EnnemyQTE.gd
+++ EnnemyQTE.gd
@@ -1,10 +1,24 @@
 extends Area2D
 
+var transi = false
+var coordChange = 0
+var i = 0
 
 func _ready():
 	pass
 
+
 func _physics_process(delta):
+	pass
+
+
+func _process(delta):
+	if transi:
+		var camera = get_node("../Camera2D")
+		camera.position -= coordChange
+		i += 1
+		if i >= 10:
+			transi = false
 	pass
 
 
@@ -12,15 +26,15 @@ func run_QTE(player : CharacterBody2D):
 	print('QTE !')
 	var camera = get_node("../Camera2D")
 	# Appliquer le zoom
-	camera.position.x = player.position.x
-	camera.position.y = player.position.y
 	var anim = get_node("../Camera2D/AnimationPlayer")
 	anim.play("cameraTransition")
 
 
-
-
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
+		transi = true
+		var camera = get_node("../Camera2D")
+		coordChange = (camera.position - body.position) / 10
 		run_QTE(body) # Replace with function body.
 		body.speed = 0 #arrete le joueur pour le QTG
+
