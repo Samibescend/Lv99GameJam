5b9e629ffc14c346bdd0fd60bfa3de4cd6aab473
Zorrino <pierrekouyoumdjian@hotmail.fr>
2023-04-16 17:56:42 +0200

    Show the input sprite

--- EnnemyQTE.gd
+++ EnnemyQTE.gd
@@ -3,7 +3,7 @@ extends Area2D
 var transi = false
 var coordChange = 0
 var i = 0
-
+var listOfSprite = ["res://assets/gauche.png","res://assets/droite.png","res://assets/haut.png","res://assets/bas.png"]
 func _ready():
 	pass
 
@@ -22,19 +22,31 @@ func _process(delta):
 	pass
 
 
-func run_QTE(player : CharacterBody2D):
-	print('QTE !')
+func animCamera(player : CharacterBody2D):
+	print('animation !')
 	var camera = get_node("../Camera2D")
 	# Appliquer le zoom
 	var anim = get_node("../Camera2D/AnimationPlayer")
 	anim.play("cameraTransition")
+	await get_tree().create_timer(1.5).timeout
+	run_QTE()
 
+func run_QTE():
+	print('QTE')
+	var QTE_sprite = get_node("QTE")
+	var rng = RandomNumberGenerator.new()
+	var rand = rng.randf_range(0,3)
+	var texture = load(listOfSprite[rand])
+
+	QTE_sprite.texture = texture
+	QTE_sprite.scale.x = 2
+	QTE_sprite.scale.y = 2
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		transi = true
 		var camera = get_node("../Camera2D")
 		coordChange = (camera.position - body.position) / 10
-		run_QTE(body) # Replace with function body.
-		body.speed = 0 #arrete le joueur pour le QTG
+		animCamera(body) # Replace with function body.
+		body.speed = 0 #arrete le joueur pour le QTE
 
